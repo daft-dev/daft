62e731af532b10363c4911bdbd2952b2107caf8b
David W. Hogg <dwhogg@gmail.com>
2012-09-23 12:04:47 -0400

    showing off a range of aspect ratios

--- examples/wordy.py
+++ examples/wordy.py
@@ -5,10 +5,10 @@ import daft
 
 if __name__ == "__main__":
     pgm = daft.PGM([4, 3], origin = [1.0, 0.5])
-    pgm.add_node(daft.Node("cloudy", r"cloudy", 3, 3, aspect=2.))
-    pgm.add_node(daft.Node("rain", r"rain", 2, 2, aspect=2.))
-    pgm.add_node(daft.Node("sprinkler", r"sprinkler", 4, 2, aspect=2))
-    pgm.add_node(daft.Node("wet", r"grass wet", 3, 1, aspect=2, observed=True))
+    pgm.add_node(daft.Node("cloudy", r"cloudy", 3, 3, aspect=1.8))
+    pgm.add_node(daft.Node("rain", r"rain", 2, 2, aspect=1.2))
+    pgm.add_node(daft.Node("sprinkler", r"sprinkler", 4, 2, aspect=2.1))
+    pgm.add_node(daft.Node("wet", r"grass wet", 3, 1, aspect=2.4, observed=True))
     pgm.add_edge("cloudy", "rain")
     pgm.add_edge("cloudy", "sprinkler")
     pgm.add_edge("rain", "wet")
