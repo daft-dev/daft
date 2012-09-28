8be43b0c47c68a849f48f6ad0130439e186d3cfc
Dan F-M <danfm@nyu.edu>
2012-09-28 11:48:12 -0400

    adding weak lensing to docs and layout tweaks

--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,28 @@
 Daft
 ====
 
+Examples
+--------
+
+.. image:: _static/examples/classic-thumb.png
+   :target: examples/classic
+
+.. image:: _static/examples/wordy-thumb.png
+   :target: examples/wordy
+
+.. image:: _static/examples/weaklensing-thumb.png
+   :target: examples/weaklensing
+
+.. image:: _static/examples/nocircles-thumb.png
+   :target: examples/nocircles
+
+.. image:: _static/examples/nogray-thumb.png
+   :target: examples/nogray
+
+
+Summary
+-------
+
 **Daft** renders probabilistic graphical models (PGMs) to any output format
 your `matplotlib <http://matplotlib.org/>`_ backend supports. This includes
 PDF, PNG and many other formats. Both directed and undirected graphs are
@@ -11,9 +33,6 @@ the internet.
 **Daft** is particularly well suited for rendering graphs to be included in
 LaTeX documents, even documents with non-trivial symbol definitions or
 macros.
-
-
-`examples/classic`_
 
 
 User Guide
--- examples/wordy.py
+++ examples/wordy.py
@@ -1,18 +1,18 @@
 from matplotlib import rc
 rc("font", family="serif", size=12)
 rc("text", usetex=True)
+
 import daft
 
-if __name__ == "__main__":
-    pgm = daft.PGM([3.6, 2.7], origin = [1.15, 0.65])
-    pgm.add_node(daft.Node("cloudy", r"cloudy", 3, 3, aspect=1.8))
-    pgm.add_node(daft.Node("rain", r"rain", 2, 2, aspect=1.2))
-    pgm.add_node(daft.Node("sprinkler", r"sprinkler", 4, 2, aspect=2.1))
-    pgm.add_node(daft.Node("wet", r"grass wet", 3, 1, aspect=2.4, observed=True))
-    pgm.add_edge("cloudy", "rain")
-    pgm.add_edge("cloudy", "sprinkler")
-    pgm.add_edge("rain", "wet")
-    pgm.add_edge("sprinkler", "wet")
-    pgm.render()
-    pgm.figure.savefig("wordy.pdf")
-    pgm.figure.savefig("wordy.png", dpi=150)
+pgm = daft.PGM([3.6, 2.7], origin=[1.15, 0.65])
+pgm.add_node(daft.Node("cloudy", r"cloudy", 3, 3, aspect=1.8))
+pgm.add_node(daft.Node("rain", r"rain", 2, 2, aspect=1.2))
+pgm.add_node(daft.Node("sprinkler", r"sprinkler", 4, 2, aspect=2.1))
+pgm.add_node(daft.Node("wet", r"grass wet", 3, 1, aspect=2.4, observed=True))
+pgm.add_edge("cloudy", "rain")
+pgm.add_edge("cloudy", "sprinkler")
+pgm.add_edge("rain", "wet")
+pgm.add_edge("sprinkler", "wet")
+pgm.render()
+pgm.figure.savefig("wordy.pdf")
+pgm.figure.savefig("wordy.png", dpi=150)
