a64b5c06db457c9075e60c4c00cc38d2fb0e66f8
David W. Hogg <dwhogg@gmail.com>
2012-09-28 21:50:19 -0400

    embolden

--- examples/thicklines.py
+++ examples/thicklines.py
@@ -3,28 +3,31 @@ T-shirt style
 =============
 
 Don't like dainty thin lines?  Need to make graphical-model-themed
-conference schwag?  Then `line_width` is the parameter for you.
+conference schwag?  Then `line_width` is the parameter for you.  Also
+check out the `preamble` option in the `matplotlib.rc` command.
 
 """
 
 from matplotlib import rc
 rc("font", family="serif", size=14)
 rc("text", usetex=True)
+rc('text.latex',
+   preamble="\usepackage{amssymb}\usepackage{amsmath}\usepackage{mathrsfs}")
 
 import daft
 
 # Instantiate the PGM.
-pgm = daft.PGM([2.3, 2.05], origin=[0.3, 0.3], line_width=3)
+pgm = daft.PGM([2.3, 2.05], origin=[0.3, 0.3], line_width=2.5)
 
 # Hierarchical parameters.
-pgm.add_node(daft.Node("alpha", r"$\alpha$", 0.5, 2, fixed=True))
-pgm.add_node(daft.Node("beta", r"$\beta$", 1.5, 2))
+pgm.add_node(daft.Node("alpha", r"$\boldsymbol{\alpha}$", 0.5, 2, fixed=True))
+pgm.add_node(daft.Node("beta", r"$\boldsymbol{\beta}$", 1.5, 2))
 
 # Latent variable.
-pgm.add_node(daft.Node("w", r"$w_n$", 1, 1))
+pgm.add_node(daft.Node("w", r"$\boldsymbol{w}_{\boldsymbol{n}}$", 1, 1))
 
 # Data.
-pgm.add_node(daft.Node("x", r"$x_n$", 2, 1, observed=True))
+pgm.add_node(daft.Node("x", r"$\boldsymbol{x}_{\boldsymbol{n}}$", 2, 1, observed=True))
 
 # Add in the edges.
 pgm.add_edge("alpha", "beta")
@@ -33,7 +36,7 @@ pgm.add_edge("w", "x")
 pgm.add_edge("beta", "x")
 
 # And a plate.
-pgm.add_plate(daft.Plate([0.5, 0.5, 2, 1], label=r"$n = 1, \cdots, N$",
+pgm.add_plate(daft.Plate([0.5, 0.5, 2, 1], label=r"$\boldsymbol{n} = \boldsymbol{1, \cdots, N}$",
     shift=-0.1))
 
 # Render and save.
