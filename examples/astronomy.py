333285b25f57017d288a02976665bcc0de93e418
David W. Hogg <dwhogg@gmail.com>
2012-10-01 19:55:41 -0400

    first draft done

--- examples/astronomy.py
+++ examples/astronomy.py
@@ -3,7 +3,7 @@ Astronomical imaging
 ====================
 
 This is a model for every pixel of every astronomical image ever
-taken.
+taken.  It is incomplete!
 
 """
 
@@ -13,18 +13,18 @@ rc("text", usetex=True)
 
 import daft
 
-pgm = daft.PGM([8, 8], origin=[0.5, 0.5], grid_unit=4., node_unit=1.4)
+pgm = daft.PGM([8, 6.75], origin=[0.5, 0.5], grid_unit=4., node_unit=1.4)
 
 # Start with the plates.
-tweak=0.01
+tweak=0.02
 pgm.add_plate(daft.Plate([1.5+tweak, 0.5+tweak, 6.0-2*tweak, 3.75-2*tweak], label=r"\huge telescope+camera+filter multiplets"))
 pgm.add_plate(daft.Plate([2.5+tweak, 1.0+tweak, 4.0-2*tweak, 2.75-2*tweak], label=r"\huge images"))
 pgm.add_plate(daft.Plate([3.5+tweak, 1.5+tweak, 2.0-2*tweak, 1.75-2*tweak], label=r"\huge pixel patches"))
-pgm.add_plate(daft.Plate([1.5+tweak, 4.25+tweak, 3.0-2*tweak, 1.75-2*tweak], label=r"\huge stars"))
-pgm.add_plate(daft.Plate([4.5+tweak, 4.25+tweak, 3.0-2*tweak, 1.75-2*tweak], label=r"\huge galaxies"))
+pgm.add_plate(daft.Plate([1.0+tweak, 4.25+tweak, 3.5-2*tweak, 1.75-2*tweak], label=r"\huge stars"))
+pgm.add_plate(daft.Plate([5.5+tweak, 4.25+tweak, 2.5-2*tweak, 1.75-2*tweak], label=r"\huge galaxies"))
 
 # ONLY pixels are observed
-asp = 2.5
+asp = 2.3
 pgm.add_node(daft.Node("true pixels", r"~\\noise-free\\pixel patch", 5.0, 2.5, aspect=asp))
 pgm.add_node(daft.Node("pixels", r"pixel patch", 4.0, 2.0, observed=True, aspect=asp))
 pgm.add_edge("true pixels", "pixels")
@@ -38,42 +38,71 @@ pgm.add_edge("sky prior", "sky")
 # Stars
 pgm.add_node(daft.Node("star patch", r"star patch", 4.0, 3.0, aspect=asp))
 pgm.add_edge("star patch", "true pixels")
-pgm.add_node(daft.Node("star SED", r"~\\spectral energy\\distribution", 3.0, 4.75, aspect=asp))
+pgm.add_node(daft.Node("star SED", r"~\\spectral energy\\distribution", 2.5, 4.75, aspect=asp))
 pgm.add_edge("star SED", "star patch")
 pgm.add_node(daft.Node("star position", r"position", 4.0, 4.75, aspect=asp))
 pgm.add_edge("star position", "star patch")
-pgm.add_node(daft.Node("temperature", r"temperature", 2.0, 5.25, aspect=asp))
+pgm.add_node(daft.Node("temperature", r"temperature", 1.5, 5.25, aspect=asp))
 pgm.add_edge("temperature", "star SED")
-pgm.add_node(daft.Node("luminosity", r"luminosity", 3.0, 5.25, aspect=asp))
+pgm.add_node(daft.Node("luminosity", r"luminosity", 2.5, 5.25, aspect=asp))
 pgm.add_edge("luminosity", "star SED")
-pgm.add_node(daft.Node("star extinction", r"extinction", 4.0, 5.25, aspect=asp))
-pgm.add_edge("star extinction", "star SED")
-pgm.add_edge("star position", "star extinction")
-pgm.add_node(daft.Node("metallicity", r"metallicity", 2.0, 5.75, aspect=asp))
+pgm.add_node(daft.Node("metallicity", r"metallicity", 1.5, 5.75, aspect=asp))
 pgm.add_edge("metallicity", "star SED")
 pgm.add_edge("metallicity", "temperature")
 pgm.add_edge("metallicity", "luminosity")
-pgm.add_node(daft.Node("mass", r"mass", 3.0, 5.75, aspect=asp))
+pgm.add_node(daft.Node("mass", r"mass", 2.5, 5.75, aspect=asp))
 pgm.add_edge("mass", "temperature")
 pgm.add_edge("mass", "luminosity")
-pgm.add_node(daft.Node("age", r"age", 4.0, 5.75, aspect=asp))
+pgm.add_node(daft.Node("age", r"age", 3.5, 5.75, aspect=asp))
 pgm.add_edge("age", "temperature")
 pgm.add_edge("age", "luminosity")
+pgm.add_node(daft.Node("star models", r"star models", 1.0, 4.0, fixed=True))
+pgm.add_edge("star models", "temperature")
+pgm.add_edge("star models", "luminosity")
+pgm.add_edge("star models", "star SED")
 
 # Galaxies
 pgm.add_node(daft.Node("galaxy patch", r"galaxy patch", 5.0, 3.0, aspect=asp))
 pgm.add_edge("galaxy patch", "true pixels")
-pgm.add_node(daft.Node("galaxy SED", r"~\\spectral energy\\distribution", 6.0, 4.75, aspect=asp))
+pgm.add_node(daft.Node("galaxy SED", r"~\\spectral energy\\distribution", 6.5, 4.75, aspect=asp))
 pgm.add_edge("galaxy SED", "galaxy patch")
-pgm.add_node(daft.Node("morphology", r"morphology", 7.0, 4.75, aspect=asp))
+pgm.add_node(daft.Node("morphology", r"morphology", 7.5, 4.75, aspect=asp))
 pgm.add_edge("morphology", "galaxy patch")
-pgm.add_node(daft.Node("galaxy position", r"~\\redshift\\ \& position", 5.0, 5.25, aspect=asp))
+pgm.add_node(daft.Node("SFH", r"~\\star-formation\\history", 7.5, 5.25, aspect=asp))
+pgm.add_edge("SFH", "galaxy SED")
+pgm.add_edge("SFH", "morphology")
+pgm.add_node(daft.Node("galaxy position", r"~\\redshift\\ \& position", 6.0, 5.25, aspect=asp))
 pgm.add_edge("galaxy position", "galaxy SED")
 pgm.add_edge("galaxy position", "morphology")
 pgm.add_edge("galaxy position", "galaxy patch")
-pgm.add_node(daft.Node("SFH", r"~\\star-formation\\history", 7.0, 5.25, aspect=asp))
-pgm.add_edge("SFH", "galaxy SED")
-pgm.add_edge("SFH", "morphology")
+pgm.add_node(daft.Node("dynamics", r"orbit structure", 6.5, 5.75, aspect=asp))
+pgm.add_edge("dynamics", "morphology")
+pgm.add_edge("dynamics", "SFH")
+pgm.add_node(daft.Node("galaxy mass", r"mass", 7.5, 5.75, aspect=asp))
+pgm.add_edge("galaxy mass", "dynamics")
+pgm.add_edge("galaxy mass", "galaxy SED")
+pgm.add_edge("galaxy mass", "SFH")
+
+# Universals
+pgm.add_node(daft.Node("extinction model", r"extinction model", 5.0, 4.75, aspect=asp))
+pgm.add_edge("extinction model", "star patch")
+pgm.add_edge("extinction model", "galaxy patch")
+pgm.add_node(daft.Node("MW", r"~\\Milky Way\\formation", 4.0, 6.5, aspect=asp))
+pgm.add_edge("MW", "metallicity")
+pgm.add_edge("MW", "mass")
+pgm.add_edge("MW", "age")
+pgm.add_edge("MW", "star position")
+pgm.add_node(daft.Node("galaxy formation", r"~\\galaxy\\formation", 5.0, 6.5, aspect=asp))
+pgm.add_edge("galaxy formation", "MW")
+pgm.add_edge("galaxy formation", "dynamics")
+pgm.add_edge("galaxy formation", "galaxy mass")
+pgm.add_node(daft.Node("LSS", r"~\\large-scale\\structure", 6.0, 6.5, aspect=asp))
+pgm.add_edge("LSS", "galaxy position")
+pgm.add_node(daft.Node("cosmology", r"~\\cosmological\\parameters", 6.0, 7.0, aspect=asp))
+pgm.add_edge("cosmology", "LSS")
+pgm.add_edge("cosmology", "galaxy formation")
+pgm.add_node(daft.Node("god", r"God", 7.0, 7.0, fixed=True))
+pgm.add_edge("god", "cosmology")
 
 # Sensitivity
 pgm.add_node(daft.Node("zeropoint", r"~\\zeropoint\\(photocal)", 3.0, 3.0, aspect=asp))
@@ -96,6 +125,11 @@ pgm.add_node(daft.Node("flatfield", r"flat-field", 2.0, 1.5, aspect=asp))
 pgm.add_edge("flatfield", "pixels")
 pgm.add_node(daft.Node("nonlinearity", r"non-linearity", 2.0, 1.0, aspect=asp))
 pgm.add_edge("nonlinearity", "pixels")
+pgm.add_node(daft.Node("WCS", r"~\\astrometric\\calibration", 3.0, 2.0, aspect=asp))
+pgm.add_edge("WCS", "star patch")
+pgm.add_edge("WCS", "galaxy patch")
+pgm.add_node(daft.Node("pointing", r"~\\telescope\\pointing etc.", 2.0, 2.0, aspect=asp))
+pgm.add_edge("pointing", "WCS")
 
 # Noise
 pgm.add_node(daft.Node("noise patch", r"noise patch", 5.0, 2.0, aspect=asp))
@@ -105,7 +139,7 @@ pgm.add_node(daft.Node("noise model", r"noise model", 7.0, 2.0, aspect=asp))
 pgm.add_edge("noise model", "noise patch")
 pgm.add_node(daft.Node("noise prior", r"noise priors", 8.0, 2.0, fixed=True))
 pgm.add_edge("noise prior", "noise model")
-pgm.add_node(daft.Node("cosmic rays", r"cosmic-ray model", 8.0, 1.5, aspect=asp))
+pgm.add_node(daft.Node("cosmic rays", r"~\\cosmic-ray\\model", 8.0, 1.5, aspect=asp))
 pgm.add_edge("cosmic rays", "noise patch")
 
 # Render and save.
